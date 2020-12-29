282d565f8c36fef9eb10b09d47bd67bcd509a4dc
spidercatnat <bloomjazzer@gmail.com>
2020-12-28 21:13:21 -0800

    Rename functions to examples

--- functions/examples/pentatonic.js
+++ functions/examples/pentatonic.js
@@ -1,6 +1,5 @@
-const { rotations } = require("../functions");
+const { rotations } = require("..");
 const { Scale } = require("@tonaljs/tonal");
 
 console.log(rotations(Scale.get("C minor pentatonic"), "parallel"));
-
 console.log(rotations(Scale.get("C minor pentatonic"), "derivative"));
