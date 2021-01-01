f34b9514d556249db6324c5daf1ced414627026a
spidercatnat <natalieb@hey.com>
2021-01-01 14:10:06 -0800

    Whoopty

--- main/index.js
+++ main/index.js
@@ -11,7 +11,7 @@ const {
  * @param {number} ambitus - The ambitus. 
  * @returns {object} - All non-redundant subsets in a given ambitus.
  * ================================================================================================
- *=================================================================================================*/
+ * ================================================================================================*/
 function getAllSubsets(ambitus) {
   // The interval the ambitus represents. 
   // Ex: Ambitus = 3 -> ["C", "D"]
