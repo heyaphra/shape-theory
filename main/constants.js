389cc96a316a2ef1aecadcc233fdc6b9eda9110b
spidercatnat <natalieb@hey.com>
2021-01-01 16:01:11 -0800

    Whoopty

--- main/constants.js
+++ main/constants.js
@@ -1,13 +1,13 @@
 const { Scale, Note: { enharmonic } } = require("@tonaljs/tonal");
 
 const FUNDAMENTAL = "C";
-const CHROMATIC_DATUM = Scale.get(`${FUNDAMENTAL} chromatic`);
-const CHROMATIC_SCALE = CHROMATIC_DATUM["notes"].map(n => enharmonic(n))
-const ALL_INTERVALS = CHROMATIC_DATUM["intervals"];
+const CHROMATIC_METADATA = Scale.get(`${FUNDAMENTAL} chromatic`);
+const CHROMATIC_SCALE = CHROMATIC_METADATA["notes"].map(n => enharmonic(n))
+const ALL_INTERVALS = CHROMATIC_METADATA["intervals"];
 
 module.exports = {
     FUNDAMENTAL,
-    CHROMATIC_DATUM,
+    CHROMATIC_METADATA,
     CHROMATIC_SCALE,
     ALL_INTERVALS
 }
